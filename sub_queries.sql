cbb7346d1a0dc15ac7e593ec0a241bbf9bf3e65a
gabriel090 <gabriel.mbatha@gmail.com>
2022-02-05 09:56:11 +0300

    Scalary subquery

--- sub_queries.sql
+++ sub_queries.sql
@@ -9,3 +9,12 @@ Question: Find the employees who's salary is more than the average salary earned
 select avg(salary) from employees;
 
 select * from employees where salary > (select avg(salary) from employees);
+
+
+--Scalary subquery--
+--it always returns 1 row and 1 column
+
+select *
+from employees e
+join (select avg(salary) sal from employees) avg_sal
+    on e.salary > avg_sal.sal;
